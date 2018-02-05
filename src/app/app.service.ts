55d2b1c01ef6deb9cb8f952072409a339cb9f71a
baggio4ever <baggio4ever@gmail.com>
2018-02-05 17:33:45 +0900

    体裁整え

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 
 const BASE_URL = 'https://o0rm1lfsej.execute-api.us-west-2.amazonaws.com/dev/';
@@ -8,69 +8,69 @@ const BASE_URL = 'https://o0rm1lfsej.execute-api.us-west-2.amazonaws.com/dev/';
 export class AppService {
     constructor(private http: HttpClient) {}
 
-    getMessage( callback:(msg:string)=>void ):void {
+    getMessage( callback: (msg: string) => void ): void {
         // API GatewayとHTTP通信して、取得成功時にコールバックを行う
-        this.http.get( BASE_URL + 'hello').subscribe(data=>{
+        this.http.get( BASE_URL + 'hello').subscribe(data => {
             callback(data['message']);
         });
     }
 
-    getMessage2( callback:(msg:string)=>void ):void {
+    getMessage2( callback: (msg: string) => void ): void {
         // API GatewayとHTTP通信して、取得成功時にコールバックを行う
-        this.http.get( BASE_URL + 'byebye').subscribe(data=>{
+        this.http.get( BASE_URL + 'byebye').subscribe(data => {
             callback(data['message']);
         });
     }
 
-    getCurrentDateTime( callback:(msg:string)=>void ):void {
+    getCurrentDateTime( callback: (msg: string) => void ): void {
         // API GatewayとHTTP通信して、取得成功時にコールバックを行う
-        this.http.get( BASE_URL + 'now').subscribe(data=>{
+        this.http.get( BASE_URL + 'now').subscribe(data => {
             callback(data['datetime']);
         });
     }
 
-    getLogsYMD( user_id:string,year:number,month:number,date:number,callback:(msg)=>void):void {
-        let y0 = ('0000' + year).slice(-4);
-        let m0 = ('00' + month).slice(-2);
-        let d0 = ('00' + date).slice(-2);
+    getLogsYMD( user_id: string, year: number, month: number, date: number, callback: (msg) => void): void {
+        const y0 = ('0000' + year).slice(-4);
+        const m0 = ('00' + month).slice(-2);
+        const d0 = ('00' + date).slice(-2);
         // Parameters obj-
         const params: HttpParams = new HttpParams().set('user_id', user_id ).set('year', y0 ).set('month', m0 ).set('date', d0 );
 
         console.log(params.toString());
 
-        this.http.get( BASE_URL + 'logs',{ params:params }).subscribe(data=>{
+        this.http.get( BASE_URL + 'logs', { params: params }).subscribe(data => {
             callback(data['logs']);
         });
     }
 
-    getLogsYM( user_id:string,year:number,month:number,callback:(msg:string)=>void):void {
-        let y0 = ('0000' + year).slice(-4);
-        let m0 = ('00' + month).slice(-2);
+    getLogsYM( user_id: string, year: number, month: number, callback: (msg: string) => void): void {
+        const y0 = ('0000' + year).slice(-4);
+        const m0 = ('00' + month).slice(-2);
         // Parameters obj-
-        let params: HttpParams = new HttpParams().set('user_id', user_id ).set('year', y0 ).set('month', m0 );
+        const params: HttpParams = new HttpParams().set('user_id', user_id ).set('year', y0 ).set('month', m0 );
 
         console.log(params.toString());
 
-        this.http.get( BASE_URL + 'logs',{ params:params }).subscribe(data=>{
+        this.http.get( BASE_URL + 'logs', { params: params }).subscribe(data => {
             callback(data['logs']);
         });
     }
 
-    getLogsY( user_id:string,year:number,callback:(msg:string)=>void):void {
-        let y0 = ('0000' + year).slice(-4);
+    getLogsY( user_id: string, year: number, callback: (msg: string) => void): void {
+        const y0 = ('0000' + year).slice(-4);
         // Parameters obj-
-        let params: HttpParams = new HttpParams().set('user_id', user_id ).set('year', y0 );
+        const params: HttpParams = new HttpParams().set('user_id', user_id ).set('year', y0 );
 
         console.log(params.toString());
 
-        this.http.get( BASE_URL + 'logs',{ params:params }).subscribe(data=>{
+        this.http.get( BASE_URL + 'logs', { params: params }).subscribe(data => {
             callback(data['logs']);
         });
     }
 
-    postMessage( body, callback:(msg:string)=>void ):void {
+    postMessage( body, callback: (msg: string) => void ): void {
         // API GatewayとHTTP通信して、取得成功時にコールバックを行う
-        this.http.post( BASE_URL + 'log',body).subscribe(data=>{
+        this.http.post( BASE_URL + 'log', body).subscribe(data => {
             callback(data['message']['message']);
         });
     }
