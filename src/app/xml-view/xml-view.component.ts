627f41bf17240ae83d3a462f07288a07216c7fdd
baggio4ever <baggio4ever@gmail.com>
2018-03-01 12:57:19 +0900

    cytoscape.js ぶっこんでみた

--- src/app/xml-view/xml-view.component.ts
+++ src/app/xml-view/xml-view.component.ts
@@ -6,7 +6,7 @@ import {
   ViewChild, ElementRef, SimpleChanges, SimpleChange } from '@angular/core';
 
 import * as vkbeautify from 'vkbeautify';
-
+import * as Cytoscape from 'cytoscape';
 
 class JdfTag {
   id: string;
@@ -485,6 +485,8 @@ export class XmlViewComponent implements OnChanges, OnInit, DoCheck,
   displayedColumns3 = ['to', 'from', 'travel'];
 //  dataSource = ELEMENT_DATA;
 
+  cy = null;
+
   constructor() { }
 
   ngOnChanges( changes: SimpleChanges ) {
@@ -516,6 +518,47 @@ export class XmlViewComponent implements OnChanges, OnInit, DoCheck,
 
   ngAfterViewInit() {
 //    console.log('AfterViewInit');
+    this.cy = Cytoscape({
+      container: document.getElementById('cy'), // container to render in
+
+      elements: [ // list of graph elements to start with
+        { // node a
+          data: { id: 'a' }
+        },
+        { // node b
+          data: { id: 'b' }
+        },
+        { // edge ab
+          data: { id: 'ab', source: 'a', target: 'b' }
+        }
+      ],
+
+      style: [ // the stylesheet for the graph
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle'
+          }
+        }
+      ],
+
+      layout: {
+        name: 'grid',
+        rows: 1
+      }
+
+    });
   }
 
   ngAfterViewChecked() {
